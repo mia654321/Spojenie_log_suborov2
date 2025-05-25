c54c6142ec4cf60f86e00987954f5dfefafad381
Maria Aronova <maria.aronova@yahoo.com>
2025-05-25 23:40:30 +0200

    Test načítania riadku, správny format

--- Test_uloha2.py
+++ Test_uloha2.py
@@ -1,9 +1,15 @@
 import unittest
+from Uloha2 import split_line_to_time_and_text
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestCaseSplitLineToTimeAndText(unittest.TestCase):
+
+    # Otestujem správne rozdelenie riadku
+    def test_line_with_time_and_text(self):
+        line = "12:23:59.969  configuration for relay"
+        time, text = split_line_to_time_and_text(line)
+        self.assertEqual(time, "12:23:59.969")
+        self.assertEqual(text, "configuration for relay")
 
 
 if __name__ == '__main__':
