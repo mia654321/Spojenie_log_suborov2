3d1fcc48e906fd994d7b01a333e5d8b20b1c1921
Maria Aronova <maria.aronova@yahoo.com>
2025-05-26 00:48:09 +0200

    Test - pridanie statusu

--- Test_uloha1.py
+++ Test_uloha1.py
@@ -1,8 +1,30 @@
 import unittest
+import xml.etree.ElementTree as ET
+from Uloha1 import update_interface_statuses
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_add_status_to_interface(self):
+        xml_data = """
+        <configuration>
+            <network>
+                <interface name="eth0">
+                    <ip>192.168.0.1</ip>
+                </interface>
+                <interface name="eth1">
+                    <ip>192.168.0.2</ip>
+                    <status>disabled</status>
+                </interface>
+            </network>
+        </configuration>
+        """
+        tree = ET.ElementTree(ET.fromstring(xml_data))
+        updated_tree = update_interface_statuses(tree)
+        root = updated_tree.getroot()
+        interfaces = root.find('network').findall('interface')
+
+        # pridám status
+        self.assertEqual(interfaces[0].find('status').text, 'enabled')
 
 if __name__ == '__main__':
     unittest.main()
